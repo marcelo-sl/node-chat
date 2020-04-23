6e6b08f3b16d37f808c23e97239850d6bdc1c533
marcelo-sl <mar.sousa2061@gmail.com>
2020-04-23 12:39:27 -0300

    Aplication Backend

---
+++ server.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utlis/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utlis/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'Node-Chat BOT';
+
+// Run when client connects
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to Node-Chat'));
+
+        // Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+            
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+
+
+    });
+
+    // Listen for chatMessages
+    socket.on('chatMessage', msg => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
